05ce8a228faf1a2aea8ce1cb22286f77687742f6
qpeterp <lsepeter0827@naver.com>
2024-11-21 10:35:53 +0900

    Feat :: CreateTeamScreen UI

---
+++ app/src/main/java/com/qpeterp/clip/domain/model/team/Team.kt
@@ -0,0 +1,6 @@
+package com.qpeterp.clip.domain.model.team
+
+data class Team(
+    val teamName: String,
+    val memberNum: Int,
+)
---
+++ app/src/main/java/com/qpeterp/clip/presentation/feature/team/viewmodel/CreateTeamViewModel.kt
@@ -0,0 +1,15 @@
+package com.qpeterp.clip.presentation.feature.team.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.qpeterp.clip.domain.model.team.Team
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class CreateTeamViewModel @Inject constructor(
+) : ViewModel() {
+    private val _teamList = MutableStateFlow<List<Team>>(listOf(Team("clip", 4)))
+    val teamList: StateFlow<List<Team>> get() = _teamList
+}
