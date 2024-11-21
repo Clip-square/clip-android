0d3e2a1d0a4872e7be6d49a5499d42bff520de02
qpeterp <lsepeter0827@naver.com>
2024-11-21 10:05:03 +0900

    Feat :: Navigation auth

--- app/src/main/java/com/qpeterp/clip/presentation/root/navigation/NavGroup.kt
+++ app/src/main/java/com/qpeterp/clip/presentation/root/navigation/NavGroup.kt
@@ -1,6 +1,14 @@
 package com.qpeterp.clip.presentation.root.navigation
 
 sealed class NavGroup {
+    data object Auth: NavGroup() {
+        const val LOGIN = "login"
+        const val REGISTER_ID = "registerId"
+        const val REGISTER_NAME = "registerName"
+        const val REGISTER_PASSWORD = "registerPassword"
+        const val REGISTER_COMPLETE = "registerComplete"
+    }
+
     data object Main: NavGroup() {
         const val SETUP = "setup"
         const val MEETING = "meeting"
--- app/src/main/java/com/qpeterp/clip/presentation/root/navigation/NavigationGraph.kt
+++ app/src/main/java/com/qpeterp/clip/presentation/root/navigation/NavigationGraph.kt
@@ -2,22 +2,52 @@ package com.qpeterp.clip.presentation.root.navigation
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.qpeterp.clip.presentation.feature.auth.register.screen.RegisterCompleteScreen
+import com.qpeterp.clip.presentation.feature.auth.register.screen.RegisterIdScreen
+import com.qpeterp.clip.presentation.feature.auth.register.screen.RegisterNameScreen
+import com.qpeterp.clip.presentation.feature.auth.register.screen.RegisterPasswordScreen
 import com.qpeterp.clip.presentation.feature.main.end.screen.EndScreen
 import com.qpeterp.clip.presentation.feature.main.meeting.screen.MeetingScreen
 import com.qpeterp.clip.presentation.feature.main.setup.screen.SetupScreen
+import com.qpeterp.clip.presentation.feature.auth.login.screen.LoginScreen
+import com.qpeterp.clip.presentation.feature.auth.register.viewmodel.RegisterViewModel
 
 @ExperimentalMaterial3Api
 @Composable
 fun NavigationGraph(
     navController: NavHostController,
+    isUserLoggedIn: Boolean,
 ) {
+    val registerViewModel: RegisterViewModel = hiltViewModel()
     NavHost(
         navController = navController,
-        startDestination = NavGroup.Main.SETUP
+        startDestination = if (isUserLoggedIn) NavGroup.Main.SETUP else NavGroup.Auth.LOGIN
     ) {
+        // Auth 그룹 네비게이션
+        composable(NavGroup.Auth.LOGIN) {
+            LoginScreen(navController)
+        }
+
+        composable(NavGroup.Auth.REGISTER_ID) {
+            RegisterIdScreen(navController, registerViewModel)
+        }
+
+        composable(NavGroup.Auth.REGISTER_PASSWORD) {
+            RegisterPasswordScreen(navController, registerViewModel)
+        }
+
+        composable(NavGroup.Auth.REGISTER_NAME) {
+            RegisterNameScreen(navController, registerViewModel)
+        }
+
+        composable(NavGroup.Auth.REGISTER_COMPLETE) {
+            RegisterCompleteScreen(navController)
+        }
+
         // Main 그룹 네비게이션
         composable(NavGroup.Main.SETUP) {
             SetupScreen(navController)
